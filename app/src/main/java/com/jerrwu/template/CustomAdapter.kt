28a09b5d66a7abff1c80059cb3319b85c50a9753
jerrwu <48305556+jerrwu@users.noreply.github.com>
2019-08-01 15:47:23 -0400

    implemented basic recyclerview

---
+++ app/src/main/java/com/jerrwu/template/CustomAdapter.kt
@@ -0,0 +1,35 @@
+package com.jerrwu.template
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.recycler_item.view.*
+
+class CustomAdapter(val userList: ArrayList<String>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    //this method is returning the view for each item in the list
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
+        return ViewHolder(v)
+    }
+
+    //this method is binding the data on the list
+    override fun onBindViewHolder(holder: CustomAdapter.ViewHolder, position: Int) {
+        holder.bindItems(userList[position])
+    }
+
+    //this method is giving the size of the list
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    //the class is hodling the list view
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindItems(user: String) {
+            itemView.rv_item.text = user
+        }
+    }
+
+}
--- app/src/main/java/com/jerrwu/template/MainActivity.kt
+++ app/src/main/java/com/jerrwu/template/MainActivity.kt
@@ -3,15 +3,22 @@ package com.jerrwu.template
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.View
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val users = ArrayList<String>()
+        users.add("user1")
+        users.add("user2")
+        users.add("user3")
+
+        val obj_adapter = CustomAdapter(users)
+
+        rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+        rv.adapter = obj_adapter
     }
 }
