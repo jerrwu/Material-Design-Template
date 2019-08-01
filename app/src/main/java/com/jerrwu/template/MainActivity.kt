a42f43012467e4ed65c91ac2409fe1bd593f1ca2
jerrwu <48305556+jerrwu@users.noreply.github.com>
2019-08-01 15:08:15 -0400

    working on recyclerview

--- app/src/main/java/com/jerrwu/template/MainActivity.kt
+++ app/src/main/java/com/jerrwu/template/MainActivity.kt
@@ -2,6 +2,8 @@ package com.jerrwu.template
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,5 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
