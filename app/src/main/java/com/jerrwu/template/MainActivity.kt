7e4a58a48b6f207a94ea8ca80a392da820effbb4
jerrwu <48305556+jerrwu@users.noreply.github.com>
2019-08-02 11:20:25 -0400

    trying to get fragments working

--- app/src/main/java/com/jerrwu/template/MainActivity.kt
+++ app/src/main/java/com/jerrwu/template/MainActivity.kt
@@ -2,23 +2,15 @@ package com.jerrwu.template
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.recyclerview_fragment.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val users = ArrayList<String>()
-        users.add("user1")
-        users.add("user2")
-        users.add("user3")
-
-        val objAdapter = CustomAdapter(users)
-
-        rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-        rv.adapter = objAdapter
     }
 }
