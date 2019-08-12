4aea616dce8381ce15ee4ac544ba4b30c316703a
jerrwu <48305556+jerrwu@users.noreply.github.com>
2019-08-12 10:41:54 -0400

    updated icon

--- app/src/main/java/com/jerrwu/template/CustomAdapter.kt
+++ app/src/main/java/com/jerrwu/template/CustomAdapter.kt
@@ -8,23 +8,19 @@ import kotlinx.android.synthetic.main.recycler_item.view.*
 
 class CustomAdapter(val userList: ArrayList<String>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
-    //this method is returning the view for each item in the list
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
         return ViewHolder(v)
     }
 
-    //this method is binding the data on the list
     override fun onBindViewHolder(holder: CustomAdapter.ViewHolder, position: Int) {
         holder.bindItems(userList[position])
     }
 
-    //this method is giving the size of the list
     override fun getItemCount(): Int {
         return userList.size
     }
 
-    //the class is hodling the list view
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bindItems(user: String) {
