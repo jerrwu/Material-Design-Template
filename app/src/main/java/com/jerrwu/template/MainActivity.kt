873bd58fcd835b9840a4e289b365ad639b6029ec
jerrwu <48305556+jerrwu@users.noreply.github.com>
2019-08-12 16:31:11 -0400

    working on dark mode toggle

--- app/src/main/java/com/jerrwu/template/MainActivity.kt
+++ app/src/main/java/com/jerrwu/template/MainActivity.kt
@@ -6,11 +6,13 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
 import android.content.SharedPreferences
-
+import android.support.v7.app.AppCompatDelegate
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -21,13 +23,5 @@ class MainActivity : AppCompatActivity() {
 
         android.support.v7.preference.PreferenceManager
             .setDefaultValues(this, R.xml.preferences, false)
-
-        val sharedPref = android.support.v7.preference.PreferenceManager
-            .getDefaultSharedPreferences(this)
-        val switchPref = sharedPref.getBoolean(getString(R.string.dark_toggle_key), false)
-        Toast.makeText(
-            this, switchPref.toString(),
-            Toast.LENGTH_SHORT
-        ).show()
     }
 }
