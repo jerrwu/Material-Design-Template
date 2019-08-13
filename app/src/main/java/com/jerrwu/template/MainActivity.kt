f7cbaba25e353fbe5e8eae27c47ed76a0cde6991
jerrwu <48305556+jerrwu@users.noreply.github.com>
2019-08-13 10:29:14 -0400

    now checks for dark mode when launching

--- app/src/main/java/com/jerrwu/template/MainActivity.kt
+++ app/src/main/java/com/jerrwu/template/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.jerrwu.template
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import android.widget.Toast
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceManager
@@ -17,7 +15,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener)
+
+        val darkToggle = sharedPreferences.getString("dark_toggle", "2")?.toInt()
+        when (darkToggle) {
+            -1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+            1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            2 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        sharedPreferences.registerOnSharedPreferenceChangeListener(darkModeListener)
 
         settingsButton.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
@@ -28,7 +35,7 @@ class MainActivity : AppCompatActivity() {
             .setDefaultValues(this, R.xml.preferences, false)
     }
 
-    var sharedPreferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener =
+    var darkModeListener: SharedPreferences.OnSharedPreferenceChangeListener =
         SharedPreferences.OnSharedPreferenceChangeListener {sharedPreferences, key ->
             if (key == "dark_toggle") {
                 val darkToggle = sharedPreferences.getString(key, "2")?.toInt()
