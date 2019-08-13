6d2f878b39ec67109225ad0c7cdbc6e909f7820a
jerrwu <48305556+jerrwu@users.noreply.github.com>
2019-08-13 10:11:49 -0400

    migrated to androidx + working dark mode toggle

--- app/src/main/java/com/jerrwu/template/MainActivity.kt
+++ app/src/main/java/com/jerrwu/template/MainActivity.kt
@@ -1,27 +1,43 @@
 package com.jerrwu.template
 
+import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
 import android.content.SharedPreferences
-import android.support.v7.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO)
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener)
 
         settingsButton.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
 
-        android.support.v7.preference.PreferenceManager
+        PreferenceManager
             .setDefaultValues(this, R.xml.preferences, false)
     }
+
+    var sharedPreferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener =
+        SharedPreferences.OnSharedPreferenceChangeListener {sharedPreferences, key ->
+            if (key == "dark_toggle") {
+                val darkToggle = sharedPreferences.getString(key, "2")?.toInt()
+                when (darkToggle) {
+                    -1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                    1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    2 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+            }
+        }
 }
--- app/src/main/java/com/jerrwu/template/SettingsFragment.kt
+++ app/src/main/java/com/jerrwu/template/SettingsFragment.kt
@@ -2,8 +2,8 @@ package com.jerrwu.template
 
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v7.preference.PreferenceFragmentCompat
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
