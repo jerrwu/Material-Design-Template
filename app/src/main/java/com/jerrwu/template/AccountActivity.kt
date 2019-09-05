1a444fefd39756b634026f768e11d251d773484b
jerrwu <48305556+jerrwu@users.noreply.github.com>
2019-09-04 22:57:48 -0400

    updating the colors on account page

--- app/src/main/java/com/jerrwu/template/AccountActivity.kt
+++ app/src/main/java/com/jerrwu/template/AccountActivity.kt
@@ -1,10 +1,24 @@
 package com.jerrwu.template
 
+import android.content.res.ColorStateList
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_account.*
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.content.ContextCompat
+
 
 class AccountActivity : AppCompatActivity() {
+    private val buttonDisabled = ColorStateList.valueOf(Color.parseColor("#363636"))
+    private val buttonEnabled = ColorStateList.valueOf(Color.parseColor("#80B4FF"))
+    private val textDisabled = Color.parseColor("#747474")
+    private val textEnabled = Color.parseColor("#242424")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +28,41 @@ class AccountActivity : AppCompatActivity() {
             finish()
         }
 
-        
+        accountEditText.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.length != 0) {
+                    accountSaveButton.isClickable = true
+
+                    if (isUsingNightModeResources()) {
+                        accountSaveButton.backgroundTintList = buttonEnabled
+                        account_check_image.setColorFilter(textEnabled)
+                        account_save_text.setTextColor(textEnabled)
+                    }
+
+                    else {
+                        accountSaveButton.backgroundTintList =
+                            ContextCompat.getColorStateList(applicationContext, R.color.colorAccent)
+                        account_check_image.setColorFilter(
+                            ContextCompat.getColor(applicationContext, R.color.colorMain))
+                        account_save_text.setTextColor(
+                            ContextCompat.getColor(applicationContext, R.color.colorMain))
+                    }
+                }
+            }
+        })
+
+    }
+    private fun isUsingNightModeResources(): Boolean {
+        return when (resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> true
+            Configuration.UI_MODE_NIGHT_NO -> false
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> false
+            else -> false
+        }
     }
 }
